825754bd7a44918040e979636ba96e9c8a8b9dc8
Suarez Rodríguez, Cristian Javier <c.suarez15@unapec.edu.do>
2025-12-01 21:01:03 -0400

    V2

--- DgiiCalculator.Application/DTOs/CalculationInputDto.cs
+++ DgiiCalculator.Application/DTOs/CalculationInputDto.cs
@@ -4,9 +4,9 @@ namespace DgiiCalculator.Application.DTOs
 {
     public class CalculationInputDto
     {
-        
         [Required(ErrorMessage = "El ingreso es obligatorio.")]
-        [Range(0.01, 999999999, ErrorMessage = "El valor debe ser mayor a 0.")]
+        // rango máximo 999 billones (15 dígitos)
+        [Range(0.01, 999999999999999, ErrorMessage = "El valor debe ser mayor a 0 y tener un formato válido.")]
         public decimal? IncomeAmount { get; set; }
 
         [Required(ErrorMessage = "Debe seleccionar una frecuencia.")]
